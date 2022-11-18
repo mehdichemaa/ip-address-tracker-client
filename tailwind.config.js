8e83a72e8468693e225d41a8fd20046a4a3520c0
mehdichemaa <chemaaelmehdi1@gmail.com>
2022-11-18 15:14:55 +0100

    finished building the UI✅

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{css,js}', './index.html', './style.css'],
+  theme: {
+    extend: {},
+    screens: {
+      tablet: '640px',
+      laptop: '1024px',
+      desktop: '1280px',
+    },
+    colors: {
+      white: '#ffffff',
+      'black-light': '#2b2b2b',
+      'black-dark': '#000000',
+      gray: '#969696',
+    },
+    fontFamily: {
+      rubik: 'rubik',
+    },
+  },
+  plugins: [require('@tailwindcss/forms')],
+};
