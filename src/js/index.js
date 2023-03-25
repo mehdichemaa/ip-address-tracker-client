3578e58cbc3bd9f91dd08502b512ca9d7a5e73f6
mehdichemaa <52676889+mehdichemaa@users.noreply.github.com>
2023-03-25 10:58:53 +0100

    chore - Changed the files and folders directories

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,14 @@
-// Leaflet JS
+// Initializing Leaflet JS
 var map = L.map('map').setView([51.505, -0.09], 13);
 
+// Adding OpenStreetMap tile layer
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
   attribution:
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
+
+L.marker([51.5, -0.09])
+  .addTo(map)
+  .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+  .openPopup();
