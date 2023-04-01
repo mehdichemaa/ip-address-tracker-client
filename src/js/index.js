ed73ea11c60a0c4d19d294b441182055d1953deb
mehdichemaa <52676889+mehdichemaa@users.noreply.github.com>
2023-04-01 17:40:20 +0200

    feat - Implemented the search feature

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,38 @@
+// Select elements and fields
+const formInput = document.querySelector('.formInput');
+const searchBtn = document.querySelector('.searchBtn');
+const ipAddressField = document.querySelector('.ipAddressField');
+const locationField = document.querySelector('.locationField');
+const timeZoneField = document.querySelector('.timeZoneField');
+const zipCodeField = document.querySelector('.zipCodeField');
+
+// Get IP Address Data
+async function getIpAddressData(ip) {
+  // Default options are marked with *
+  const response = await fetch(`http://127.0.0.1:3000/?ip=${ip}`)
+    .then((response) => response.json())
+    .then((data) => {
+      ipAddressField.textContent = ip;
+      locationField.textContent = data.result.city + ', ' + data.result.country;
+      timeZoneField.textContent = data.result.time_zone;
+      zipCodeField.textContent = data.result.zip_code;
+
+      console.log('Success:', data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
+
+// Fetch data when clicking search
+searchBtn.addEventListener('click', async (event) => {
+  event.preventDefault();
+
+  if (formInput.value) {
+    getIpAddressData(formInput.value);
+  }
+});
+
 // Initializing Leaflet JS
 var map = L.map('map').setView([51.505, -0.09], 13);
 
